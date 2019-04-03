f8360a5e3fceacce7a77e0c746bb61a45596ed1e
PavlMais <pavlomajstruk@gmail.com>
2019-04-03 17:30:09 +0300

    --

--- bashTerminal/core.sh
+++ bashTerminal/core.sh
@@ -1,1 +1,15 @@
 #! usr/bin/bash
+files=(modules/*)
+i=1
+for n in "${files[@]}" 
+do
+  echo "$i > $n " 
+  cat $n | grep desc
+  i=$(( $i + 1 )) 
+done 
+
+read sel
+
+source ${files[$sel + 1]]}
+
+
